b2be4b79cb4e033e30c7ff0c10efd190a7d31ab5
Nick Reardon <32754140+ThingOfNightmare@users.noreply.github.com>
2020-01-31 15:22:39 -0800

    Add project files.

---
+++ CS1D AS3 StacksQueuesDequeus/main.cpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * AUTHOR 	  		: Nick Reardon
+ * Assignment #3	: Stacks, Queues, Dequeus
+ * CLASS			: CS1D
+ * SECTION			: MW - 2:30p
+ * DUE DATE			: 02 / 03 / 20
+ *****************************************************************************/
+#include "main.h"
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+
+	/*
+	 * HEADER OUTPUT
+	 */
+	PrintHeader(cout, "Prompt.txt");
+
+	/********************************************************************/
+
+
+
+	system("pause");
+	return 0;
+}
